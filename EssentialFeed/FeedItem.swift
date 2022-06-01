d281ae971c619b24f661bf6b0b0262c405a3d2fe
Renz <salvador.rhenz@gmail.com>
2022-06-01 22:18:14 +0800

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by John Salvador on 6/1/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
